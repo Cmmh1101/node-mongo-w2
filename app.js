0be9090d93c016167233a1e172cdb47ef28b6f58
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-09-18 13:19:00 -0500

    week 4

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-
 var logger = require("morgan");
-
 const passport = require("passport");
 const config = require("./config");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-// new routers
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 const uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favoriteRouter");
 
 const mongoose = require("mongoose");
-const { array } = require("yargs");
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -33,6 +30,7 @@ connect.then(
 
 var app = express();
 
+// Secure traffic only
 app.all("*", (req, res, next) => {
   if (req.secure) {
     return next();
@@ -54,7 +52,7 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser("12345-67890-09876-54321"));
+//app.use(cookieParser('12345-67890-09876-54321'));
 
 app.use(passport.initialize());
 
@@ -62,13 +60,12 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 app.use(express.static(path.join(__dirname, "public")));
-// new app.use
+
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/imageUpload", uploadRouter);
+app.use("/favorites", favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
